85d8d5983c770b05cd7186438417283dd0288124
Abhishek Jain <abhishekjain1416@gmail.com>
2023-11-14 20:30:03 +0530

    Feat: Update User Online Offline Status in MongoDB

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const userRoute = require('./routes/userRoute');
 
 app.use('/',userRoute);
 
+const User = require('./models/userModel');
+
+const io = require('socket.io')(http);
+
+var usp = io.of('/user-namespace');
+
+usp.on('connection', async function(socket){
+    console.log('User connected.');
+
+    var userId = socket.handshake.auth.token;
+    await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'1'}});
+
+    socket.on('disconnect', async function(){
+        console.log('User disconnected.');
+        await User.findByIdAndUpdate({_id:userId}, {$set:{is_online:'0'}});
+    });
+});
+
 http.listen(3000,function(){
     console.log('Server is running...');
 });
