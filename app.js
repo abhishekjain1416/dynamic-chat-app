400753a0acd759f1822cac0e66758234bd4d1f87
Abhishek Jain <abhishekjain1416@gmail.com>
2023-11-16 23:55:50 +0530

    Feat: Load Old Chats from MongoDB using Socket.IO

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const userRoute = require('./routes/userRoute');
 app.use('/',userRoute);
 
 const User = require('./models/userModel');
+const Chat = require('./models/chatModel');
 
 const io = require('socket.io')(http);
 
@@ -37,6 +38,16 @@ usp.on('connection', async function(socket){
 
     socket.on('newChat', function(data){
         socket.broadcast.emit('loadNewChat', data);
+    });
+
+    // Load old chats from MongoDB
+    socket.on('existsChats', async function(data){
+        var chats = await Chat.find({$or:[
+            {sender_id:data.sender_id, receiver_id:data.receiver_id},
+            {sender_id:data.receiver_id, receiver_id:data.sender_id}
+        ]});
+
+        socket.emit('loadChats', { chats: chats });
     });
 });
 
